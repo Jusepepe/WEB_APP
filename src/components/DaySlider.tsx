a9e03af5d1dd736006f8689ba11103aea01860c6
Jusepepe <saltamontehz@gmail.com>
2025-05-26 17:06:21 -0500

    Mejorando slider de días

--- src/components/DaySlider.tsx
+++ src/components/DaySlider.tsx
@@ -3,8 +3,6 @@ import { LeftArrow, RightArrow } from "./Arrows.tsx";
 import { DayButton } from "./DayButton.tsx";
 
 const monthNames = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-const weekNames = ['Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab'];
-const weekDays = [0, 1, 2, 3, 4, 5, 6];
 
 export function DaySlider() {   
     const [day, setDay] = useState(new Date())
@@ -22,11 +20,13 @@ export function DaySlider() {
             <nav class="flex flex-row bg-gray-800 rounded-lg">
                 <LeftArrow changeDay={changeDay} change={-7}/>
                 <div class="flex flex-row overflow-x-scroll">
-                    <DayButton day={day} changeDay={changeDay} change={-2} selected={false}/>
                     <DayButton day={day} changeDay={changeDay} change={-1} selected={false}/>
                     <DayButton day={day} changeDay={changeDay} change={0} selected={true}/>
                     <DayButton day={day} changeDay={changeDay} change={1} selected={false}/>
                     <DayButton day={day} changeDay={changeDay} change={2} selected={false}/>
+                    <DayButton day={day} changeDay={changeDay} change={3} selected={false}/>
+                    <DayButton day={day} changeDay={changeDay} change={4} selected={false}/>
+                    <DayButton day={day} changeDay={changeDay} change={5} selected={false}/>
                 </div>
                 <RightArrow changeDay={changeDay} change={7}/>
             </nav>
